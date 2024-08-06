8d4656e4b4fd6ec6e3a20866f55d5ef859434938
AnPham79 <anpnb79@gmail.com>
2024-08-06 23:58:38 +0700

    post get with firebase axios

---
+++ http request/src/components/survey/UserExperiences.vue
@@ -0,0 +1,64 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
+      </div>
+      <ul>
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+// import axios from 'axios';
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  components: {
+    SurveyResult,
+  },
+  data() {
+    return {
+      results : []
+    }
+  },
+  methods : {
+    loadExperiences() {
+        fetch('https://learn-vue-808a8-default-rtdb.firebaseio.com/surveys.json')
+        .then((res) => {
+            if(res.ok) {
+                return res.json();
+            }
+        })
+        .then((data) => {
+            const results = [];
+            for( const id in data ) {
+                results.push({
+                    id : id,
+                    name : data[id].name,
+                    rating : data[id].rating
+                })
+            }
+            this.results = results
+        })
+        .catch()
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
